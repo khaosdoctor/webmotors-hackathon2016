380322c769832421c1ad88f245cd06440dfae4d4
Lucas Santos <lhs.santoss@gmail.com>
2016-10-23 02:50:36 -0200

    Comparação FUNCIONANDO

--- modules/ger.js
+++ modules/ger.js
@@ -1,16 +1,17 @@
 let moment = require('moment');
 
+
 module.exports = {
   //Adds new events to the list
   addEvent: (redis, namespace, person, thing) => {
     let d = new Date();
-    let event = [{
+    let event = {
       namespace: namespace,
       person: person,
       action: 'clicks',
       thing: thing,
       expires_at: moment(d).add(1, 'months').format('YYYY-MM-DD')
-    }];
+    };
     return redis.rpush("events:" + namespace, JSON.stringify(event), (err, res) => {
       if (err) {
         console.error(err);
@@ -20,16 +21,17 @@ module.exports = {
     });
   },
   getRecommendations: (ger, events, namespace, person) => {
-     return ger.initialize_namespace('carros')
+    return ger.initialize_namespace('carros')
       .then(() => {
         return ger.events(events);
       })
       .then(() => {
-        return ger.recommendations_for_person(namespace, person, { actions: { 'clicks': 1 }, time_until_expiry: 2592000, filter_previous_actions: ["clicks"]});
+        // What things might alice like?
+        return ger.recommendations_for_person('carros', 'alice', { actions: { clicks: 1 } , time_until_expiry: 2592000, filter_previous_actions: ["clicks"]})
       })
-      .then((recommend) => {
-        return (recommend);
-       });
+      .then((recommendations) => {
+          return recommendations.recommendations;
+      });
   }
 };
 
